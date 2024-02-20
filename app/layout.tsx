f8d5e51a92ac8e8e1ec6c23d04f5db66c55ba48b
Prateek Kashyap <kashyapprateek13@gmail.com>
2024-02-20 10:44:47 +0530

    init: layout created

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+
+import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-poppins",
+});
+
+export const metadata: Metadata = {
+  title: "Summit Spot",
+  description: "Summit Spot is the best place to find your next event.",
+  icons: { icon: "/assets/light.png" },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={poppins.variable}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
+}
